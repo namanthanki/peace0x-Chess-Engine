b0219283fc93610ee0814436efd7349f220f761e
Naman Thanki <namanthanki@users.noreply.github.com>
2021-01-27 10:54:14 +0530

    tested main

--- peace0x.c
+++ peace0x.c
@@ -36,7 +36,7 @@ int main(void) {
     movelist newList[1];
 
     parseFEN("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", newBoard);
-    perftTest(4, newBoard);
+    perftTest(5, newBoard);
 
     return 0;
 }
