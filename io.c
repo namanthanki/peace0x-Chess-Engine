a51b9a4b9c8fb5ac581d0d9a78880d36208e1c80
Naman Thanki <namanthanki@users.noreply.github.com>
2021-01-28 10:55:42 +0530

    added parseMove function

--- io.c
+++ io.c
@@ -1,4 +1,5 @@
 #include "definitions.h" 
+#include "validate.h"
 
 char *printSquareString(const int square) {
     static char squareString[3];
@@ -34,6 +35,53 @@ char *printMove(const int move) {
     }
 
     return moveString;
+}
+
+int parseMove(char *ptrChar, board *position) {
+    if(ptrChar[1] > '8' || ptrChar[1] < '1') {
+        return NOMOVE;
+    }
+    if(ptrChar[3] > '8' || ptrChar[3] < '1') {
+        return NOMOVE;
+    }
+    if(ptrChar[0] > 'h' || ptrChar[0] < 'a') {
+        return NOMOVE;
+    }
+    if(ptrChar[2] > 'h' || ptrChar[2] < 'a') {
+        return NOMOVE;
+    }
+
+    int from = FILE_RANK_TO_SQUARE(ptrChar[0] - 'a', ptrChar[1] - '1');
+    int to = FILE_RANK_TO_SQUARE(ptrChar[2] - 'a', ptrChar[3] - '1');
+
+    ASSERT(squareOnBoard(from) && squareOnBoard(to));
+
+    movelist newList[1];
+    generateAllMoves(position, newList);
+    int moveNumber = 0;
+    int move = NOMOVE;
+    int promotedPiece = EMPTY_SQR;
+    
+    for(moveNumber = 0; moveNumber < newList -> numberOfMoves; moveNumber++) {
+        move = newList -> moves[moveNumber].move;
+        if(FROM(move) == from && TOSQ(move) == to) {
+            promotedPiece = PROMOTED(move);
+            if(promotedPiece != EMPTY_SQR) {
+                if(isRQ(promotedPiece) && !isBQ(promotedPiece) && ptrChar[4] == 'r') {
+                    return move;
+                }else if(!isRQ(promotedPiece) && isBQ(promotedPiece) && ptrChar[4] == 'b') {
+                    return move;
+                }else if(isRQ(promotedPiece) && isBQ(promotedPiece) && ptrChar[4] == 'q') {
+                    return move;
+                }else if(isKn(promotedPiece) && ptrChar[4] == 'n') {
+                    return move;
+                }
+                continue;
+            }
+            return move;
+        }
+    }
+    return NOMOVE;
 }
 
 void printMoveList(const movelist *list) {
