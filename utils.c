cb0984127d3d3928570bdb56bb97629350d213b7
Naman Thanki <namanthanki@users.noreply.github.com>
2021-01-28 10:58:46 +0530

    now can get the system time in ms for linux and windows

---
+++ utils.c
@@ -0,0 +1,17 @@
+#include "definitions.h"
+
+#ifdef WIN32
+#include <windows.h>
+#else
+#include <sys/time.h>
+#endif
+
+int getTimeInMiliseconds() {
+    #ifdef WIN32   
+        return GetTickCount();
+    #else 
+        struct timeval t;
+        gettimeofday(&t, NULL);
+        return t.tv_sec * 1000  + t.tv_usec / 1000;
+    #endif
+}
