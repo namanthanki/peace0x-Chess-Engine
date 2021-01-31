6a06a9e71a952e6148641bede19fb4b6c1256f3e
Naman Thanki <namanthanki@users.noreply.github.com>
2021-01-31 23:40:05 +0530

    transportation table

--- pvtable.c
+++ pvtable.c
@@ -1,11 +1,11 @@
 #include "include/definitions.h"
 
-const int pvSize = 0x100000 * 2;
+const int hashSize = 0x100000 * 16;
 
 int getPvLine(const int depth, board *position) {
     ASSERT(depth < MAXDEPTH);
 
-    int move = probePvTable(position);
+    int move = probePvMove(position);
     int numOfMovesinArr = 0;
 
     while(move != NOMOVE && numOfMovesinArr < depth) {
@@ -18,7 +18,7 @@ int getPvLine(const int depth, board *position) {
             break;
         }
 
-        move = probePvTable(position);   
+        move = probePvMove(position);   
     }
 
     while(position -> ply > 0) {
@@ -27,40 +27,118 @@ int getPvLine(const int depth, board *position) {
     return numOfMovesinArr;
 }
 
-void clearPvTable(pvTable *table) {
-    pvEntry *p_pvEntry;
-    for(p_pvEntry = table -> pTable; p_pvEntry < table -> pTable + table -> numberOfEntries; p_pvEntry++) {
-        p_pvEntry -> hashKey = 0ULL;
-        p_pvEntry -> move = NOMOVE;
+void clearHashTable(hashTable *table) {
+    hashEntry *p_hashEntry;
+    for(p_hashEntry = table -> pTable; p_hashEntry < table -> pTable + table -> numberOfEntries; p_hashEntry++) {
+        p_hashEntry -> hashKey = 0ULL;
+        p_hashEntry -> move = NOMOVE;
+        p_hashEntry -> depth = 0;
+        p_hashEntry -> score = 0;
+        p_hashEntry -> flags = 0;
     }
+
+    table -> newWrite = 0;
 }
 
-void initPvTable(pvTable *table) {
-    table -> numberOfEntries = pvSize / sizeof(pvEntry);
+void initHashTable(hashTable *table) {
+    table -> numberOfEntries = hashSize / sizeof(hashEntry);
     table -> numberOfEntries -= 2;
     if (table->pTable != NULL) {
         free(table->pTable);
     }
-    table -> pTable = (pvEntry *) malloc(table -> numberOfEntries * sizeof(pvEntry));
-    clearPvTable(table);
+    table -> pTable = (hashEntry *) malloc(table -> numberOfEntries * sizeof(hashEntry));
+    clearHashTable(table);
     printf("pvTable init complete with %d entries\n", table -> numberOfEntries);
 }
 
-void storePvMove(const board *position, const int move) {
-    int index = position -> hashKey  % position -> newPvTable -> numberOfEntries; 
-    ASSERT(index >= 0 && index <= position -> newPvTable -> numberOfEntries - 1);
+void storeHashEntry(board *position, const int move, int score, const int flags, const int depth) {
+    int index = position -> hashKey  % position -> hashTable -> numberOfEntries; 
+    ASSERT(index >= 0 && index <= position -> hashTable -> numberOfEntries - 1);
+    ASSERT(depth >= 1 && depth < MAXDEPTH);
+    ASSERT(flags >= HASH_FLAG_UPPER && flags <= HASH_FLAG_EXACT);
+    ASSERT(score >= -INFINITE && score <= INFINITE);
+    ASSERT(position -> ply >= 0 && position -> ply < MAXDEPTH);
 
-    position -> newPvTable -> pTable[index].move = move;
-    position -> newPvTable -> pTable[index].hashKey = position -> hashKey;
-}
-
-int probePvTable(const board *position) {
-    int index = position -> hashKey  % position -> newPvTable -> numberOfEntries; 
-    ASSERT(index >= 0 && index <= position -> newPvTable -> numberOfEntries - 1);
-
-    if(position -> newPvTable -> pTable[index].hashKey == position -> hashKey) {
-        return position -> newPvTable -> pTable[index].move;
+    if(position -> hashTable -> pTable[index].hashKey == 0) {
+        position -> hashTable -> newWrite++;
+    }else {
+        position -> hashTable -> overWrite++;
     }
 
+    if(score > ISMATE) {
+        score += position -> ply;
+    }else if(score < -ISMATE) {
+        score -= position -> ply;
+    }
+
+    position -> hashTable -> pTable[index].move = move;
+    position -> hashTable -> pTable[index].hashKey = position -> hashKey;
+    position -> hashTable -> pTable[index].flags = flags;
+    position -> hashTable -> pTable[index].score = score;
+    position -> hashTable -> pTable[index].depth = depth;
+}
+
+int probeHashEntry(board *position, int *move, int *score, int alpha, int beta, int depth) {
+    int index = position -> hashKey  % position -> hashTable -> numberOfEntries; 
+    
+    ASSERT(index >= 0 && index <= position -> hashTable -> numberOfEntries - 1);
+    ASSERT(depth >= 1 && depth < MAXDEPTH);
+    ASSERT(alpha < beta);
+    ASSERT(alpha >= -INFINITE && alpha <= INFINITE);
+    ASSERT(beta >= -INFINITE && beta <= INFINITE);
+    ASSERT(position -> ply >= 0 && position -> ply < MAXDEPTH);
+
+    if(position -> hashTable -> pTable[index].hashKey == position -> hashKey) {
+        *move = position -> hashTable -> pTable[index].move;
+        if(position -> hashTable -> pTable[index].depth >= depth) {
+            position -> hashTable -> hit++;
+
+            ASSERT(position -> hashTable -> pTable[index].depth >= 1 && position -> hashTable -> pTable[index].depth < MAXDEPTH);
+            ASSERT(position -> hashTable -> pTbale[index].flags >= HASH_FLAG_ALPHA && position -> hashTable -> pTable[index].flags <= HASH_FLAG_EXACT);
+            
+            *score = position->hashTable -> pTable[index].score;
+            if(*score > ISMATE) {
+                *score -= position -> ply;
+            }else if(*score < -ISMATE) {
+                *score += position -> ply;
+            }
+
+            switch(position -> hashTable -> pTable[index].flags) {
+                ASSERT(*score >= -INFINITE && *score <= INFINITE);
+
+                case HASH_FLAG_ALPHA:
+                    if(*score <= alpha) {
+                        *score = alpha;
+                        return TRUE;
+                    }
+                    break;
+
+                case HASH_FLAG_BETA:
+                    if(*score >= beta) {
+                        *score = beta;
+                        return TRUE;
+                    }
+                    break;
+
+                case HASH_FLAG_EXACT: 
+                    return TRUE;
+                    break;
+
+                default:
+                    ASSERT(FALSE); 
+                    break;
+            }
+        }
+    }
+    return FALSE;
+}
+
+int probePvMove(const board *position) {
+    int index = position -> hashKey % position -> hashTable -> numberOfEntries;
+    ASSERT(index >= 0; && index <= position->hashTable->numberOfEntries - 1);
+    
+    if(position -> hashTable -> pTable[index].hashKey == position -> hashKey) {
+        return position -> hashTable -> pTable[index].move;
+    }
     return NOMOVE;
 }
